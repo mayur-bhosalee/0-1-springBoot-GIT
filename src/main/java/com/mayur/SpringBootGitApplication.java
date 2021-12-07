1c9db453880f9f226bb47e7d90c19bf0b5c212e0
mayu <payal bhosale>
2021-12-07 08:39:33 +0530

    update task HIS-301

--- src/main/java/com/mayur/SpringBootGitApplication.java
+++ src/main/java/com/mayur/SpringBootGitApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootGitApplication {
 
-	public static void main(String[] args)
+	public static void main(String[] args)String name="Sourav";
 
 	{ 
-    String name="mayur bhosale"; String name="payal bhosdale";
+    String name="mayur bhosale"; String name="payal bhosdale"; String name="payal bhosdale";
 
 
 	{
