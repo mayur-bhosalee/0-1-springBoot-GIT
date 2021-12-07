54bcaab5c0d241e2164d88839543380ab50815d5
mayu <payal bhosale>
2021-12-07 07:55:39 +0530

    upoad new prioject

---
+++ src/main/java/com/mayur/SpringBootGitApplication.java
@@ -0,0 +1,16 @@
+package com.mayur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitApplication {
+
+	public static void main(String[] args)
+	{
+		final String url="http://localost";
+		
+		SpringApplication.run(SpringBootGitApplication.class, args);
+	}
+
+}
